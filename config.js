7c43d1da2ca4c4d6590ff064d03c7aae7764f343
portalwifidrthiess <vladmocan1998@gmail.com>
2026-07-21 12:19:16 +0300

    Fix syntax for APPS_SCRIPT_URL in config.js
    Updated APPS_SCRIPT_URL to use quotes for proper syntax.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // 1. Adresa Web App-ului Google Apps Script (vezi apps-script.gs / README.md
 //    pentru instrucțiuni de publicare). Arată cam așa:
 //    https://script.google.com/macros/s/AKfycb.../exec
-const APPS_SCRIPT_URL = https://script.google.com/macros/s/AKfycbwKNns7zNNeSnVDaZx3Ah_nRZD1T7RXYF4M_qhq1-7kXeBKfk0cdK9wxmtvN7eIBXJL/exec;
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwKNns7zNNeSnVDaZx3Ah_nRZD1T7RXYF4M_qhq1-7kXeBKfk0cdK9wxmtvN7eIBXJL/exec";
 
 // 2. Firmele și angajații fiecăreia.
 const COMPANIES = [
